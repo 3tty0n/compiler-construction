fe0f97e6e093695e64b43a9f6a25d0ce303a976a
YuIzawa <yuizalp@gmail.com>
2016-11-29 18:00:47 +0900

    Update Eval.scala

--- exp-sbt/src/Eval.scala
+++ exp-sbt/src/Eval.scala
@@ -3,13 +3,17 @@ import Base._
 
 object Eval {
   def eval (env: Map[Var, Int], e: Exp) : Int =
-    e match {
-      case VarExp(x) => env(x)
-      case IntExp(i) => i
-      case PlusExp(e1, e2) => eval (env, e1) + eval (env, e2)
-      case MinusExp(e1, e2) => eval (env, e1) - eval (env, e2)
-      case TimesExp(e1, e2) => eval (env, e1) * eval (env, e2)
-      case DevideExp(e1, e2) => eval (env, e1) / eval (env, e2)
+    try {
+      e match {
+        case VarExp(x) => env(x)
+        case IntExp(i) => i
+        case PlusExp(e1, e2) => eval (env, e1) + eval (env, e2)
+        case MinusExp(e1, e2) => eval (env, e1) - eval (env, e2)
+        case TimesExp(e1, e2) => eval (env, e1) * eval (env, e2)
+        case DevideExp(e1, e2) => eval (env, e1) / eval (env, e2)
+      }
+    } catch {
+      case _: NoSuchElementException => 0
     }
 }
 
