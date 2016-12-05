efcf5e53f4b29c8a4c5905a17c82976140362779
YuIzawa <yuizalp@gmail.com>
2016-12-05 21:36:28 +0900

    Create LexSpec.scala

---
+++ lex-sbt/test/LexSpec.scala
@@ -0,0 +1,37 @@
+import java.io.StringReader
+
+import Base.MyError
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+
+import scala.sys.process._
+import Tokens._
+
+class LexSpec
+  extends FlatSpec
+    with Matchers
+    with BeforeAndAfterAll
+{
+
+  override def beforeAll(): Unit = {
+    Process("java -jar lex-sbt/lib/jflex-scala.jar --scala -d lex-sbt/src lex-sbt/flex/Sample.flex").run()
+    Thread.sleep(1000)
+  }
+
+  "flex" should "正しく正規表現を定義できている" in {
+    new Yylex(new StringReader("1")).yylex() should equal (NUM(1))
+    new Yylex(new StringReader("if")).yylex() should equal (IF)
+    new Yylex(new StringReader("123.14")).yylex() should equal (REAL(123.14))
+  }
+
+  it should "0から始まる数字は0以外 Exception を送出する" in {
+    intercept[MyError] {
+      new Yylex(new StringReader("01")).yylex()
+    }
+  }
+
+  it should "予約語を `` で囲むと識別子として使用できる" in {
+    new Yylex(new StringReader("`if`")).yylex() should equal (ID("if"))
+  }
+
+
+}
