5f8710f8041dea21420a2a4bc62ec3adbe79c393
YuIzawa <yuizalp@gmail.com>
2016-12-12 19:21:31 +0900

    Implement toDFA

--- automata-sbt/src/Automata.scala
+++ automata-sbt/src/Automata.scala
@@ -1,14 +1,14 @@
 class DFA[Q, A](
-  val states: Set[Q],
-  val alpha: Set[A],
-  val transition: Map[(Q,A),Q],
-  val q0: Q,
-  val finalStates: Set[Q]) {
+                 val states: Set[Q],
+                 val alpha: Set[A],
+                 val transition: Map[(Q, A), Q],
+                 val q0: Q,
+                 val finalStates: Set[Q]) {
 
   def trans(q: Q, w: List[A]): Q =
     w match {
       case Nil => q
-      case a::w => trans(transition(q,a), w)
+      case a :: w => trans(transition(q, a), w)
     }
 
   def accept(w: List[A]) =
@@ -19,15 +19,15 @@ class DFA[Q, A](
     }
 }
 
-class NFA[Q,A](
-  val states: Set[Q],
-  val alpha: Set[A],
-  t: Map[(Q,Option[A]),Set[Q]],          // εをNoneで表現
-  val q0: Q,
-  val finalStates: Set[Q]) {
+class NFA[Q, A](
+                 val states: Set[Q],
+                 val alpha: Set[A],
+                 t: Map[(Q, Option[A]), Set[Q]], // εをNoneで表現
+                 val q0: Q,
+                 val finalStates: Set[Q]) {
 
   val transition = t.withDefaultValue(Set())
-                   // キーに対して値が定義されていないときに空集合を返す
+  // キーに対して値が定義されていないときに空集合を返す
 
   def eclosure(aQs: Set[Q]) = {
     var qs = Set[Q]()
@@ -35,32 +35,42 @@ class NFA[Q,A](
     while (newQs != qs) {
       qs = newQs
       for (q <- qs) newQs = newQs ++ transition((q, None))
-    } 
+    }
     qs
   }
 
   def transSet(qs: Set[Q], w: List[A]): Set[Q] =
     w match {
       case Nil => eclosure(qs)
-      case a::w => transSet(eclosure(qs).flatMap(q => transition((q,Some(a)))),w)
+      case a :: w => transSet(eclosure(qs).flatMap(q => transition((q, Some(a)))), w)
     }
 
   def trans(q: Q, w: List[A]): Set[Q] = transSet(Set(q), w)
 
   def accept(w: List[A]) =
-      !(trans(q0, w) & finalStates).isEmpty
+    (trans(q0, w) & finalStates).nonEmpty
 
-  def toDFA(): DFA[Set[Q],A] = {
-    val q0DFA =eclosure(Set(q0))
+  def toDFA(): DFA[Set[Q], A] = {
+    val q0DFA = eclosure(Set(q0))
     var statesDFA = Set(q0DFA)
-    var u = List(q0DFA)            // リストをスタックとして使用
+    var u = List(q0DFA)
+    // リストをスタックとして使用
     var transitionDFA = Map[(Set[Q], A), Set[Q]]()
 
-//    while (!u.isEmpty) {
-//       ...
-//     }
+    while (u.nonEmpty) {
+      val s = u.head
+      u = u.filter(_ != s)
+      alpha.foreach { a =>
+        val sPrime = eclosure(transSet(s, List(a)))
+        transitionDFA ++= Map((s, a) -> sPrime)
+        if (!statesDFA.contains(sPrime)) {
+          statesDFA += sPrime
+          u = sPrime :: u
+        }
+      }
+    }
     new DFA(statesDFA, alpha, transitionDFA, q0DFA,
-      statesDFA.filter(qs => !(qs & finalStates).isEmpty))
+      statesDFA.filter(qs => (qs & finalStates).nonEmpty))
   }
 }
 
