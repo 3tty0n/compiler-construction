c4c809045e65fe7a64bdf708cda6c24e9aa17e37
Yusuke Izawa <yuizalp@gmail.com>
2016-12-19 22:03:29 +0900

    Fix ParserNonscala

--- parsing-sbt/src/ParserNonscala.scala
+++ parsing-sbt/src/ParserNonscala.scala
@@ -6,41 +6,67 @@ import Tokens._
 
 class ParserNonscala (val src: Yylex) {
 
-  var tok: Token = src.yylex
+  var tok: Token = src.yylex()
 
-  def advance () =  tok = src.yylex
+  def advance(): Unit =  tok = src.yylex()
 
-  def eat (t: Token) = 
+  def eat(t: Token): Unit  =
     if (tok == t) advance() else error()
 
 
   def F(): Exp =    // 拡張が必要
     tok match {
-      case NIL => advance(); NilExp
-      case INT(i) => advance(); IntExp(i)
-      case ID(s) => advance(); VarExp(s)
+      case NIL =>
+        advance(); NilExp
+      case INT(i) =>
+        advance(); IntExp(i)
+      case ID(s) =>
+        advance(); VarExp(s)
       case LPAREN =>
-        {
-          eat(LPAREN)
-          val e = E()
-          eat(RPAREN)
-          e
-        }
-      case _ => error()
+        eat(LPAREN)
+        val e = E()
+        eat(RPAREN)
+        e
+      case _ =>
+        error(
+          message = s"expected NIL, num, (E), id, but actual $tok"
+        )
     }
 
   def T(): Exp =
     tok match {
-      case ID(_) | INT(_) | LPAREN | NIL =>  Tprime(F())
-      case _ => error()
+      case ID(_) | INT(_) | LPAREN | NIL =>
+        TPrime(F())
+      case _ =>
+        error(
+          message = s"expected id, int, (, Nil, but actual $tok"
+        )
     }
 
-  def Tprime(e: Exp): Exp =
+  def TPrime(e: Exp): Exp =
     tok match {
-      case TIMES => eat(TIMES); Tprime(BOpExp(TimesOp, e, F()))
-      case DIV => eat(DIV); Tprime(BOpExp(DivideOp, e, F()))
-      case PLUS | MINUS| RPAREN | EOF | ELSE | EQEQ | LESS | COLONCOLON => e
-      case _ => error()
+      case TIMES =>
+        eat(TIMES); TPrime(BOpExp(TimesOp, e, F()))
+      case DIV =>
+        eat(DIV); TPrime(BOpExp(DivideOp, e, F()))
+      case PLUS | MINUS| RPAREN | EOF | ELSE | EQEQ | LESS | COLONCOLON =>
+        e
+      case _ =>
+        error(
+          message = s"expected +, -, (, EOF, else, ==, <, ::, but actual $tok"
+        )
+    }
+
+  def U(): Ty =
+    tok match {
+      case ID("Int") =>
+        IntTy
+      case ID("Boolean") =>
+        BoolTy
+      case ID("List[Int]") =>
+        IntListTy
+      case _ =>
+        error(message = "expected Int, Boolean, List[Int]")
     }
 
   def E(): Exp = T()  // プログラムを書く．補助関数も必要
@@ -55,12 +81,12 @@ class ParserNonscala (val src: Yylex) {
 
   def Ds(): List[Def] = {
     tok match {
-      case DEF => {
+      case DEF =>
         val d = D()
         val ds = Ds()
-        d::ds
-      }
-      case EOF => Nil
+        d :: ds
+      case EOF =>
+        Nil
     }
   }
 }
