2453f7e96f2f717e3313d4d442baa7207fcb7a51
YuIzawa <yuizalp@gmail.com>
2016-12-25 23:06:45 +0900

    Implement tcheck

--- typing-sbt/src/Tcheck.scala
+++ typing-sbt/src/Tcheck.scala
@@ -1,4 +1,5 @@
 package nonscala
+
 import Base._
 import Abssyn._
 import Oper._
@@ -6,46 +7,89 @@ import Ty._
 
 object TypeCheck {
 
-  class TypeError(s: String) extends Throwable
-  def typeError(s: String) = throw(new TypeError(s))
+  class TypeError(message: String = null, cause: Throwable = null)
+  extends RuntimeException(message, cause)
+
+  def typeError(m: String) = throw new TypeError(m)
 
   case class FuncTy(ts: List[Ty], result: Ty)
 
   // 式 e の型を返す
   // 型エラーがあった場合は, 例外 TypeError を発生させる
   // tcheck
-  def tcheck (fenv: Map[Var, FuncTy], env: Map[Var, Ty], e: Exp) : Ty =
-    e match {
-      case VarExp(x) =>
-        env.get(x) match {
-          case Some(t) => t
-          case None => typeError("Cannot find variable: "+x)
-        }
-      case UOpExp(o, e1) => {
-        val t1 = tcheck (fenv, env, e1)
-          (o, t1) match {
-          case (IsEmptyOp, IntListTy) => BoolTy
-          case (HeadOp, IntListTy) => IntTy
-          case (TailOp, IntListTy) => IntListTy
-          case _ => typeError("UOpExp")
-        }
+  def tCheck(fenv: Map[Var, FuncTy], env: Map[Var, Ty], e: Exp): Ty =
+  e match {
+    case VarExp(x) => env.getOrElse(x, throw new TypeError(s"Cannot find variable: $x"))
+    case IntExp(_) => IntTy
+    case NilExp => IntListTy
+    case UOpExp(o, e1) =>
+      val t1 = tCheck(fenv, env, e1)
+      (o, t1) match {
+        case (IsEmptyOp, IntListTy) => BoolTy
+        case (HeadOp, IntListTy) => IntTy
+        case (TailOp, IntListTy) => IntListTy
+        case _ => throw new TypeError("UOpExp")
       }
-/*      case AppExp(f, es) => {
-        val FuncTy(ts,t) = fenv(f)
-        ... 
-        // Listのmapを使うと簡単            
-      }  */
+    case AppExp(f, es) => fenv(f).result
+    case BOpExp(o, e1, e2) =>
+      o match {
+        case PlusOp | MinusOp | TimesOp | DivideOp =>
+          val message = "type error. +, -, *, / should (Int) +, -, *, / (Int)"
+          (e1, e2) match {
+            case (IntExp(_), IntExp(_)) => IntTy
+            case (VarExp(x1), IntExp(_)) =>
+              env.get(x1) match {
+                case Some(IntTy) => IntTy
+                case _ => throw new TypeError(message)
+              }
+            case (IntExp(_), VarExp(x)) =>
+              env.get(x) match {
+                case Some(IntTy) => IntTy
+                case _ => throw new TypeError(message)
+              }
+            case (VarExp(_), VarExp(_)) =>
+              (tCheck(fenv, env, e1), tCheck(fenv, env, e2)) match {
+                case (IntTy, IntTy) => IntTy
+                case _ => throw new TypeError(message)
+              }
+            case _ => throw new TypeError(message)
+          }
+        case EqOp | LtOp =>
+          (tCheck(fenv, env, e1), tCheck(fenv, env, e2)) match {
+            case (IntTy, IntTy) => BoolTy
+            case _ => throw new TypeError("EqOp, LtOp")
+          }
+        case ConsOp =>
+          val t1 = tCheck(fenv, env, e1)
+          val t2 = tCheck(fenv, env, e2)
+          (t1, t2) match {
+            case (IntTy, IntListTy) => IntListTy
+            case _ => throw new TypeError("ConsOp")
+          }
+      }
+    case IfExp(exp, e1, e2) =>
+      tCheck(fenv, env, exp) match {
+        case BoolTy => tCheck(fenv, env, e1)
+        case _ => tCheck(fenv, env, e2)
+      }
+  }
 
-    }
+  def defs2fenv(ds: List[Def]): Map[Var, FuncTy] = {
+    ds.map { d =>
+      (d.name, FuncTy(d.args.map(_._2), d.rtype))
+    }.toMap
+  }
 
-  def defs2fenv (ds: List[Def]): Map[Var, FuncTy] =
-    ds.map(d =>
-      (d.name, FuncTy(d.args.map(_._2), d.rtype))).toMap
+  def defs2env(ds: List[Def]): Map[Var, Ty] = {
+    ds.flatMap(_.args).map { arg =>
+      (arg._1, arg._2)
+    }.toMap
+  }
 
   // 型エラーがあった場合は, 例外 TypeError を発生させる
-  def tcheckDefs (ds: List[Def]): Unit = {
+  def tCheckDefs(ds: List[Def]): Unit = {
     val fenv = defs2fenv(ds)
-    for (d <- ds)
-      tcheck(fenv, Map[Var, Ty](), d.body)
+    val env: Map[Var, Ty] = defs2env(ds)
+    ds.map(d => tCheck(fenv, env, d.body))
   }
 }
--- typing-sbt/test/Tcheck.scala
+++ typing-sbt/test/Tcheck.scala
@@ -8,46 +8,78 @@ import Ty._
 import TypeCheck._
 
 class TcheckTest extends FlatSpec {
-  "変数" should "正しい型付け" in
-  {
-    assert(tcheck(Map(), Map("x"->IntTy), VarExp("x"))== IntTy)
+  "変数" should "正しい型付け" in {
+    assert(tCheck(Map(), Map("x" -> IntTy), VarExp("x")) === IntTy)
     assertThrows[TypeError] {
-      tcheck(Map(), Map(), VarExp("x"))
+      tCheck(Map(), Map(), VarExp("x"))
     }
   }
 
-  "isEmpty" should "正しい型付け" in
-  {
-    assert(tcheck(Map(), Map("x"->IntListTy),
-      UOpExp(IsEmptyOp, VarExp("x")))== BoolTy)
+  "isEmpty" should "正しい型付け" in {
+    assert(tCheck(
+      Map(),
+      Map("x" -> IntListTy),
+      UOpExp(IsEmptyOp, VarExp("x"))
+    ) === BoolTy)
     assertThrows[TypeError] {
-      tcheck(Map(), Map("x"->IntTy),
-        UOpExp(IsEmptyOp, VarExp("x")))
+      tCheck(
+        Map(),
+        Map("x" -> IntTy),
+        UOpExp(IsEmptyOp, VarExp("x"))
+      )
     }
   }
 
-  "+" should "正しい型付け" in
-  {
-    assert(tcheck(Map(), Map("x"->IntTy),
-      BOpExp(PlusOp, IntExp(1), VarExp("x")))== IntTy)
-    assertThrows[TypeError] {
-      tcheck(Map(), Map("x"->BoolTy),
-        BOpExp(PlusOp, IntExp(1), VarExp("x")))
+  "+" should "正しい型付け" in {
+    assert(tCheck(
+      Map(),
+      Map("x" -> IntTy),
+      BOpExp(PlusOp, IntExp(1), VarExp("x"))
+    ) === IntTy)
+    intercept[TypeError] {
+      tCheck(
+        Map(),
+        Map("x" -> BoolTy),
+        BOpExp(PlusOp, IntExp(1), VarExp("x"))
+      )
     }
   }
 
-  "関数" should "正しい型付け" in
-  {
-    assert(
-      tcheck(Map("f"->FuncTy(List(IntTy,BoolTy),IntListTy)),
-        Map("x"->BoolTy),
-        AppExp("f", List(IntExp(1), VarExp("x"))))== IntListTy)
+  "関数" should "正しい型付け" in {
+    assert(tCheck(
+      Map("f" -> FuncTy(List(IntTy, BoolTy), IntListTy)),
+      Map("x" -> BoolTy),
+      AppExp("f", List(IntExp(1), VarExp("x")))
+    ) === IntListTy)
   }
 
-  "例: sort" should "正しい型付け" in
-  {
-    val ds = Main.parseFileDefs("examples/sort.scala")
-    tcheckDefs(ds)
+  "if" should "正しい型付け" in {
+    assert(tCheck(
+      Map(),
+      Map("x" -> IntTy),
+      IfExp(BOpExp(EqOp, VarExp("x"), IntExp(2)), IntExp(1), IntExp(2))
+    ) === IntTy)
+  }
+
+  "例: sort" should "正しい型付け" in {
+    val ds = Main.parseFileDefs("typing-sbt/examples/sort.scala")
+    tCheckDefs(ds)
+    succeed
+  }
+
+  "例: insert" should "正しく型付けできる" in {
+    val exp =
+      """
+        |def insert(x: Int, l: List[Int]): List[Int] =
+        |  if (l.isEmpty)
+        |    x::Nil
+        |  else if (x<l.head)
+        |    x::l
+        |  else
+        |    l.head::insert(x, l.tail)
+      """.stripMargin
+    val ds = Main.parseStrDefs(exp)
+    tCheckDefs(ds)
     succeed
   }
 
