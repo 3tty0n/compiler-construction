df454ad5d76be6cb54cb67be9503574e71f7f7a3
Yusuke Izawa <yuizalp@gmail.com>
2017-01-19 02:12:32 +0900

    change == -> ===

--- codegen/test/IL.scala
+++ codegen/test/IL.scala
@@ -14,18 +14,18 @@ class ILTest extends FlatSpec {
     evalVal(exec(fenv, env, cv._1), cv._2)
 
   "変数" should "正しい値" in {
-    assert(evalCV(Map(), Map("x" -> IntValue(1)), IL.trans(VarExp("x"))) == IntValue(1))
+    assert(evalCV(Map(), Map("x" -> IntValue(1)), IL.trans(VarExp("x"))) === IntValue(1))
   }
 
   "+" should "正しい値" in {
     assert(evalCV(Map(), Map("x" -> IntValue(1)),
-      IL.trans(BOpExp(PlusOp, IntExp(1), VarExp("x")))) == IntValue(2))
+      IL.trans(BOpExp(PlusOp, IntExp(1), VarExp("x")))) === IntValue(2))
   }
 
   "算術式" should "正しい値" in {
     assert(
       evalCV(Map(), Map("x" -> IntValue(1), "y" -> IntValue(2)), IL.trans(BOpExp(PlusOp, BOpExp(TimesOp, IntExp(1), VarExp("x")), BOpExp(TimesOp, VarExp("y"), VarExp("y")))))
-      == IntValue(5))
+      === IntValue(5))
   }
 
   "関数適用" should "正しい値" in {
@@ -33,7 +33,7 @@ class ILTest extends FlatSpec {
     assert(
       evalCV(Map("f" -> FValue(List("x", "y"), c, v)),
         Map("x" -> ListValue(List(2))),
-        IL.trans(AppExp("f", List(IntExp(1), VarExp("x"))))) == ListValue(List(1, 2)))
+        IL.trans(AppExp("f", List(IntExp(1), VarExp("x"))))) === ListValue(List(1, 2)))
   }
 
   "例: arith (x-y) * z" should "正しい値" in {
@@ -44,7 +44,7 @@ class ILTest extends FlatSpec {
     }
       val fenv = defs2env(ds)
       assert(
-        evalCV(fenv, Map(), IL.trans(AppExp("test", List(IntExp(4), IntExp(2), IntExp(3))))) == IntValue(6))
+        evalCV(fenv, Map(), IL.trans(AppExp("test", List(IntExp(4), IntExp(2), IntExp(3))))) === IntValue(6))
   }
 
   "例: fact" should "正しい値" in {
@@ -56,7 +56,7 @@ class ILTest extends FlatSpec {
 
     val fenv = defs2env(ds)
     assert(
-      evalCV(fenv, Map(), IL.trans(AppExp("fact", List(IntExp(4))))) == IntValue(24))
+      evalCV(fenv, Map(), IL.trans(AppExp("fact", List(IntExp(4))))) === IntValue(24))
   }
 
 
@@ -68,7 +68,7 @@ class ILTest extends FlatSpec {
     }
     val fenv = defs2env(ds)
     assert(
-      evalCV(fenv, Map("x" -> ListValue(List(3, 2, 1))), IL.trans(AppExp("sort", List(VarExp("x"))))) == ListValue(List(1, 2, 3)))
+      evalCV(fenv, Map("x" -> ListValue(List(3, 2, 1))), IL.trans(AppExp("sort", List(VarExp("x"))))) === ListValue(List(1, 2, 3)))
 
   }
 
@@ -80,7 +80,7 @@ class ILTest extends FlatSpec {
     }
     val fenv = defs2env(ds)
     assert(
-      evalCV(fenv, Map("x" -> ListValue(List(3, 2, 1, 3, 5, 3, 1, 4))), IL.trans(AppExp("qsort", List(VarExp("x"))))) == ListValue(List(1, 1, 2, 3, 3, 3, 4, 5)))
+      evalCV(fenv, Map("x" -> ListValue(List(3, 2, 1, 3, 5, 3, 1, 4))), IL.trans(AppExp("qsort", List(VarExp("x"))))) === ListValue(List(1, 1, 2, 3, 3, 3, 4, 5)))
 
   }
 
@@ -92,7 +92,7 @@ class ILTest extends FlatSpec {
     }
     val fenv = defs2env(ds)
     assert(
-      evalCV(fenv, Map(), IL.trans(AppExp("primes", List(IntExp(20))))) ==
+      evalCV(fenv, Map(), IL.trans(AppExp("primes", List(IntExp(20))))) ===
         ListValue(List(2, 3, 5, 7, 11, 13, 17, 19)))
 
   }
